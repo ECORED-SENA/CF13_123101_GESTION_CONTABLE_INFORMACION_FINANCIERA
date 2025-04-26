0a63087b5885e26f620cc9d535f783b0842017f5
Cielo Angulo <cielodamarisangulo@gmail.com>
2025-04-25 20:26:39 -0500

    Maquetación finalizada

--- src/config/global.js
+++ src/config/global.js
@@ -1,17 +1,22 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    Name: 'Ajustes y cierre del periodo',
+    Description:
+      'En este componente se desarrollan los ajustes contables, para generar los estados financieros de propósito general de acuerdo con la normativa legal vigente. Cabe resaltar, que la información contable y financiera, debe ser coherente con los hechos económicos realizados por la empresa en cumplimiento de su objeto social; por ende, debe expresar la realidad económica atendiendo a las Normas Internacionales de Información Financiera (NIIF) Al finalizar el periodo contable se realiza el cierre del ejercicio, teniendo en cuenta los ajustes contables, cuyo objetivo es corregir aquellas partidas que no quedaron contabilizadas correctamente o en su defecto se deben saldar, con el fin de mostrar la situación económica del ente económico.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+        imagen: require('@/assets/curso/portada/a1.svg'),
       },
       {
         clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+        imagen: require('@/assets/curso/portada/a2.svg'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/a3.svg'),
       },
     ],
   },
@@ -31,28 +36,50 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Ajustes de cierre',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Deterioro',
             hash: 't_1_1',
           },
+          {
+            numero: '1.2',
+            titulo: 'Depreciaciones',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Amortización',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo: 'Diferencia en cambio',
+            hash: 't_1_4',
+          },
+          {
+            numero: '1.5',
+            titulo: 'Arqueo de caja',
+            hash: 't_1_5',
+          },
+          {
+            numero: '1.6',
+            titulo: 'Conciliación bancaria',
+            hash: 't_1_6',
+          },
+          {
+            numero: '1.7',
+            titulo: 'Balance de prueba',
+            hash: 't_1_7',
+          },
+          {
+            numero: '1.8',
+            titulo: 'Balance ajustado',
+            hash: 't_1_8',
+          },
         ],
-      },
-
-      {
-        nombreRuta: 'tema2',
-        numero: '2',
-        titulo: 'Tema 2',
-        desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'tema3',
-        numero: '3',
-        titulo: 'Tema 3',
-        desarrolloContenidos: true,
       },
     ],
     subMenu: [
